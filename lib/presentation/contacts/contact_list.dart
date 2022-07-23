46a5aba66a45cbb9a9f3c2605926ca1c9f45994a
pranavpv773 <pranavpv773@gmail.com>
2022-07-23 17:30:54 +0530

    floating button added

--- lib/presentation/contacts/contact_list.dart
+++ lib/presentation/contacts/contact_list.dart
@@ -7,53 +7,63 @@ class ContactList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 10),
-      child: ListView.builder(
-        shrinkWrap: true,
-        itemBuilder: (context, index) {
-          return Column(
-            children: [
-              InkWell(
-                onTap: () {},
-                child: ListTile(
-                  title: Text(
-                    info[index]['name'].toString(),
-                    style: const TextStyle(
-                      fontSize: 18,
-                    ),
-                  ),
-                  subtitle: Padding(
-                    padding: const EdgeInsets.only(top: 6.0),
-                    child: Text(
-                      info[index]['message'].toString(),
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: tabColor,
+        onPressed: () {},
+        child: const Icon(
+          Icons.message,
+          color: Colors.white,
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 10),
+        child: ListView.builder(
+          shrinkWrap: true,
+          itemBuilder: (context, index) {
+            return Column(
+              children: [
+                InkWell(
+                  onTap: () {},
+                  child: ListTile(
+                    title: Text(
+                      info[index]['name'].toString(),
                       style: const TextStyle(
-                        fontSize: 15,
+                        fontSize: 18,
+                      ),
+                    ),
+                    subtitle: Padding(
+                      padding: const EdgeInsets.only(top: 6.0),
+                      child: Text(
+                        info[index]['message'].toString(),
+                        style: const TextStyle(
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                        info[index]['profilePic'].toString(),
+                      ),
+                    ),
+                    trailing: Text(
+                      info[index]['time'].toString(),
+                      style: const TextStyle(
+                        fontSize: 13,
+                        color: Colors.grey,
                       ),
                     ),
                   ),
-                  leading: CircleAvatar(
-                    backgroundImage: NetworkImage(
-                      info[index]['profilePic'].toString(),
-                    ),
-                  ),
-                  trailing: Text(
-                    info[index]['time'].toString(),
-                    style: const TextStyle(
-                      fontSize: 13,
-                      color: Colors.grey,
-                    ),
-                  ),
                 ),
-              ),
-              const Divider(
-                color: dividerColor,
-                indent: 85,
-              )
-            ],
-          );
-        },
-        itemCount: info.length,
+                const Divider(
+                  color: dividerColor,
+                  indent: 85,
+                )
+              ],
+            );
+          },
+          itemCount: info.length,
+        ),
       ),
     );
   }
