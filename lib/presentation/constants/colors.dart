c467776272c7966621b023486d4404f521475484
pranavpv773 <pranavpv773@gmail.com>
2022-08-17 20:40:48 +0530

    message floating action and status scroll completed

--- lib/presentation/constants/colors.dart
+++ lib/presentation/constants/colors.dart
@@ -1,5 +1,7 @@
 import 'dart:ui';
 
+import 'package:flutter/material.dart';
+
 const backgroundColor = Color.fromRGBO(19, 28, 33, 1);
 const textColor = Color.fromRGBO(241, 241, 242, 1);
 const appBarColor = Color.fromRGBO(31, 44, 52, 1);
@@ -11,3 +13,5 @@ const searchBarColor = Color.fromRGBO(50, 55, 57, 1);
 const dividerColor = Color.fromRGBO(37, 45, 50, 1);
 const chatBarMessage = Color.fromRGBO(30, 36, 40, 1);
 const mobileChatBoxColor = Color.fromRGBO(31, 44, 52, 1);
+const kGrey = Colors.grey;
+const kWidth10 = SizedBox(width: 10);
--- lib/presentation/mobile/contacts/view/info.dart
+++ lib/presentation/mobile/contacts/view/info.dart
@@ -1,99 +1,99 @@
 const info = [
   {
-    'name': 'Rivaan Ranawat',
+    'name': 'Pranav pv',
     'message': 'Hey, how are you doing?',
-    'time': '3:53 pm',
+    'time': '1:23 pm',
     'profilePic':
         'https://upload.wikimedia.org/wikipedia/commons/8/85/Elon_Musk_Royal_Society_%28crop1%29.jpg',
   },
   {
-    'name': 'John Doe',
+    'name': 'Bhavya',
     'message': 'Hello, whats up',
-    'time': '2:25 pm',
+    'time': '12:45 pm',
     'profilePic':
         'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0ZL7it9GAAc4a1Fb40d6fxu-paaRZ-zG2yQ&usqp=CAU',
   },
   {
-    'name': 'Naman Ranawat',
+    'name': 'Sahal Brototype',
     'message': 'Hello, I want to sleep.',
-    'time': '1:03 pm',
+    'time': '12:03 pm',
     'profilePic':
         'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0ZL7it9GAAc4a1Fb40d6fxu-paaRZ-zG2yQ&usqp=CAU',
   },
   {
-    'name': 'Dad',
+    'name': 'Vaisakh Brototype',
     'message': 'Call me, have some work',
-    'time': '12:06 pm',
+    'time': '12:00 pm',
     'profilePic':
         'https://pbs.twimg.com/profile_images/1419974913260232732/Cy_CUavB.jpg',
   },
   {
-    'name': 'Mom',
+    'name': 'Anoop Amaze',
     'message': 'You ate food?',
-    'time': '10:00 am',
+    'time': '11:59 am',
     'profilePic':
         'https://uploads.dailydot.com/2018/10/olli-the-polite-cat.jpg?auto=compress%2Cformat&ixlib=php-3.3.0',
   },
   {
-    'name': 'Jurica',
+    'name': 'Ashim Brototype',
     'message': 'Yo!!!!! Long time, no see!?',
-    'time': '9:53 am',
+    'time': '11:53 am',
     'profilePic':
         'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
   },
   {
-    'name': 'Albert Dera',
+    'name': 'Vishnu Brototype',
     'message': 'Am I fat?',
-    'time': '7:25 am',
+    'time': '11:25 am',
     'profilePic':
         'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
   },
   {
-    'name': 'Joseph',
+    'name': 'Rithvik Brototype',
     'message': 'I am from International Olym...',
-    'time': '6:02 am',
+    'time': '11:02 am',
     'profilePic':
         'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
   },
   {
-    'name': 'Sikandar',
+    'name': 'Musthak Flutter Brototype',
     'message': 'Lets Code!',
-    'time': '4:56 am',
+    'time': '10:56 am',
     'profilePic':
         'https://images.unsplash.com/photo-1619194617062-5a61b9c6a049?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHJhbmRvbSUyMHBlb3BsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60',
   },
   {
-    'name': 'Ian Dooley',
+    'name': 'Vishnu Flutter Brototype',
     'message': 'Images by Unsplash',
-    'time': '1:00 am',
+    'time': '10:30 am',
     'profilePic':
         'https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
   },
   {
-    'name': 'Prithvi',
+    'name': 'Say Flutter Brototype',
     'message': 'Images by Unsplash',
-    'time': '1:00 am',
+    'time': '10:09 am',
     'profilePic':
         'https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
   },
   {
-    'name': 'Mohanlal',
+    'name': 'Salih Flutter Brototype',
     'message': 'Images by Unsplash',
-    'time': '1:00 am',
+    'time': '10:06 am',
     'profilePic':
         'https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
   },
   {
-    'name': 'Mamootty',
+    'name': 'Jishnu Flutter Brototype',
     'message': 'Images by Unsplash',
-    'time': '1:00 am',
+    'time': '10:02 am',
     'profilePic':
         'https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
   },
   {
-    'name': 'Dulquer salman',
+    'name': 'Harshad Flutter Brototype',
     'message': 'Images by Unsplash',
-    'time': '1:00 am',
+    'time': '9:50 am',
     'profilePic':
         'https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
   },
