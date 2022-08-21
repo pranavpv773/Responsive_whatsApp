c91de41b1f3e2b713218c266467923c2bc8d2850
pranavpv773 <pranavpv773@gmail.com>
2022-08-21 22:42:26 +0530

    agree and continue folder added

---
+++ lib/app/mobile/agree_continue/view/agree_continue.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class WelcomeAgree extends StatelessWidget {
+  const WelcomeAgree({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
